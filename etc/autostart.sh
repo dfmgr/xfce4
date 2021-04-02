741a2c32863381170c48d5d428c9aaa61f05e4b9
Jason <jason@casjay.net>
2021-04-02 04:13:08 -0400

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- etc/autostart.sh
+++ etc/autostart.sh
@@ -94,7 +94,7 @@ fi
 
 if __cmd_exist sxhkd; then
   __kill sxhkd
-  __start sxhkd -c "$HOME/.config/sxhkd/sxhkdrc"
+  __start sxhkd -c "$HOME/.config/sxhkd/sxhkdrc" -s "$HOME/.cache/sxhkd"
 fi
 
 if __cmd_exist setxkbmap; then
