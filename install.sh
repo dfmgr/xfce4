424b9f61ad68de1f6cff6896d9d019f0b4809377
Jason <jason@casjay.net>
2021-04-06 21:47:09 -0400

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- install.sh
+++ install.sh
@@ -171,6 +171,10 @@ if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
   execute "fontmgr install --all" "Installing fonts"
   execute "iconmgr install N.I.B." "Installing icons"
   execute "thememgr install Arc-Pink-Dark" "Installing themes"
+fi
+printf_question_timeout "$ICON_QUESTION Should I install grub customizations?"
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+  sudo true
   execute "sudo systemmgr install grub" "Installing system packages"
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
